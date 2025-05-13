f4eaa2be29cc9e73bea8b0be7af5dfc2cd97e1c7
RaulDiazR <rauldiaz.rom@gmail.com>
2025-05-12 23:53:05 -0600

    Improve MIPs format

--- main.c
+++ main.c
@@ -7,7 +7,7 @@
 #define NUM_THREADS 18
 #define NUM_IMAGENES 100
 
-void formatNumberWithCommas(unsigned long long num, char *buffer);
+void formatNumberWithCommas(const char *numStr, char *buffer);
 
 int main() {
     omp_set_num_threads(NUM_THREADS); // Se definen los threads a utilizar
@@ -89,52 +89,75 @@ int main() {
     double tiempoTotal = (double)(end - start) / CLOCKS_PER_SEC;
 
     unsigned long long totalAccesos = totalLecturas + totalEscrituras;
-    long double instrucciones = (long double)totalAccesos * 20.0 * NUM_THREADS;
+    /* Se realiza el cálculo de instrucciones, utilizando el total de accesos,
+    factor de 20.0 por líneas de código en ensamblador, número de threads y área del kernel */
+    long double instrucciones = (long double)totalAccesos * 20.0 * NUM_THREADS; // * (kernelSize * kernelSize)
+    // Se calcula el millón de instrucciones por segundo
     long double mips = (instrucciones / 1e6) / tiempoTotal;
 
-    char bufferLecturas[32];
-    char bufferEscrituras[32];
+    char lecturasStr[32], escriturasStr[32], bufferLecturas[32], bufferEscrituras[32];
+    char mipsStr[64], formattedMips[64];
     int minutos = (int)(tiempoTotal / 60);
     double segundos = tiempoTotal - (minutos * 60);  
     
-    formatNumberWithCommas(totalLecturas, bufferLecturas);
-    formatNumberWithCommas(totalEscrituras, bufferEscrituras);
+    sprintf(lecturasStr, "%llu", totalLecturas);
+    sprintf(escriturasStr, "%llu", totalEscrituras);
+    sprintf(mipsStr, "%.2Lf", mips);
+    formatNumberWithCommas(lecturasStr, bufferLecturas);
+    formatNumberWithCommas(escriturasStr, bufferEscrituras);
+    formatNumberWithCommas(mipsStr, formattedMips);
     
     fprintf(log, "\n--- Reporte Final ---\n");
     fprintf(log, "Kernel utilizado para desenfoque: %d\n", kernelSize);
     fprintf(log, "Total de localidades leídas: %s\n", bufferLecturas);
     fprintf(log, "Total de localidades escritas: %s\n", bufferEscrituras);
     fprintf(log, "Tiempo total de ejecución: %d minutos con %.2f segundos\n", minutos, segundos);
-    fprintf(log, "MIPS estimados: %.2Lf\n", mips);
+    fprintf(log, "MIPS estimados: %s\n", formattedMips);
 
     fclose(log);
 
     printf("\n\nProcesamiento terminado.\n");
     printf("Tiempo total: %d minutos con %.2f segundos\n", minutos, segundos);
-    printf("MIPS estimados: %.2Lf\n", mips);
+    printf("MIPS estimados: %s\n", formattedMips);
 
     return 0;
 }
 
-void formatNumberWithCommas(unsigned long long num, char *buffer) {
-    char temp[32];
-    sprintf(temp, "%llu", num);
+void formatNumberWithCommas(const char *numStr, char *buffer) {
+    char intPart[64], decPart[64] = "";
+    char temp[64];
 
-    int len = strlen(temp);
+    const char *dot = strchr(numStr, '.');
+    if (dot) {
+        size_t intLen = dot - numStr;
+        strncpy(intPart, numStr, intLen);
+        intPart[intLen] = '\0';
+        strncpy(decPart, dot + 1, sizeof(decPart) - 1);
+    } else {
+        strcpy(intPart, numStr);
+    }
+
+    int len = strlen(intPart);
     int commas = (len - 1) / 3;
     int newLen = len + commas;
-    buffer[newLen] = '\0';
+    temp[newLen] = '\0';
 
     int i = len - 1;
     int j = newLen - 1;
     int count = 0;
 
     while (i >= 0) {
-        buffer[j--] = temp[i--];
+        temp[j--] = intPart[i--];
         count++;
         if (count == 3 && i >= 0) {
-            buffer[j--] = ',';
+            temp[j--] = ',';
             count = 0;
         }
+    }
+
+    if (strlen(decPart) > 0) {
+        snprintf(buffer, 64, "%s.%s", temp, decPart);
+    } else {
+        strcpy(buffer, temp);
     }
 }
