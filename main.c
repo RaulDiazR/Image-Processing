7583f3528fbf090424ff4913a5fdd334fdcfb1e7
Raúl Díaz Romero <116688830+RaulDiazR@users.noreply.github.com>
2025-05-24 03:33:18 -0600

    Add working version of main.c

--- main.c
+++ main.c
@@ -10,8 +10,8 @@
 #include <dirent.h>
 #include <sys/stat.h>
 
-#define NUM_IMAGENES 100
-#define NUM_THREADS 6 // Per process/thread
+#define NUM_IMAGENES 10
+#define NUM_THREADS 5 // Per process/thread
 
 void formatNumberWithCommas(const char *numStr, char *buffer);
 
@@ -28,10 +28,15 @@ int main(int argc, char *argv[]) {
     int kernelSize;
 
     if (rank == 0) {
-        // Only master reads kernel size
+        char input[16];
         do {
             printf("Ingrese el tamaño del kernel (impar entre 3 y 155): ");
-            scanf("%d", &kernelSize);
+            fflush(stdout);  // Make sure it prints before blocking
+            if (!fgets(input, sizeof(input), stdin)) {
+                fprintf(stderr, "Error reading input\n");
+                MPI_Abort(MPI_COMM_WORLD, 1);
+            }
+            kernelSize = atoi(input);
         } while (kernelSize < 3 || kernelSize > 155 || kernelSize % 2 == 0);
     }
 
@@ -40,7 +45,7 @@ int main(int argc, char *argv[]) {
 
     // Open individual log file per rank
     char logFilename[64];
-    snprintf(logFilename, sizeof(logFilename), "log_rank%d.txt", rank);
+    snprintf(logFilename, sizeof(logFilename), "./individual_logs/log_rank%d.txt", rank);
     FILE *log = fopen(logFilename, "w");
     if (!log) {
         perror("No se pudo abrir el archivo de log");
@@ -55,7 +60,7 @@ int main(int argc, char *argv[]) {
 
     unsigned long long totalLecturas = 0;
     unsigned long long totalEscrituras = 0;
-
+    int processedImgs = 0;
     clock_t startTime = clock();
 
     for (int i = start; i <= end; i++) {
@@ -70,21 +75,24 @@ int main(int argc, char *argv[]) {
         snprintf(salidaDesenfoque, sizeof(salidaDesenfoque), "./processed/image%d_blur_kernelSize%d.bmp", i, kernelSize);
         snprintf(salidaGrises, sizeof(salidaGrises), "./processed/image%d_gris.bmp", i);
 
-        unsigned long lecturas = 0, escrituras = 0;
+        unsigned long lecturas = 0, escrituras = 0, lecturasBlur = 0;
 
         invertirHorizontalGrises(entrada, salidaHorizontalGrises, log, &lecturas, &escrituras);
         invertirHorizontalColor(entrada, salidaHorizontalColor, log, &lecturas, &escrituras);
         invertirVerticalGrises(entrada, salidaVerticalGrises, log, &lecturas, &escrituras);
         invertirVerticalColor(entrada, salidaVerticalColor, log, &lecturas, &escrituras);
-        aplicarDesenfoqueIntegral(entrada, salidaDesenfoque, kernelSize, log, &lecturas, &escrituras);
+        aplicarDesenfoqueIntegral(entrada, salidaDesenfoque, kernelSize, log, &lecturasBlur, &escrituras);
         convertirAGrises(entrada, salidaGrises, log, &lecturas, &escrituras);
 
-        totalLecturas += lecturas;
+        totalLecturas += lecturas + (lecturasBlur * kernelSize * kernelSize);
         totalEscrituras += escrituras;
 
-        printf("Rango %d: Procesado imagen %d/%d\n", rank, i, end);
+        processedImgs += 1;
+        printf("\rRango %d procesando imagen %d/%d      ", rank, processedImgs, end - start + 1);
         fflush(stdout);
     }
+
+    printf("\n");
 
     clock_t endTime = clock();
     double localTime = (double)(endTime - startTime) / CLOCKS_PER_SEC;
@@ -106,13 +114,51 @@ int main(int argc, char *argv[]) {
 
     fclose(log);
 
-    // Optional: Gather and print summary from all processes
+    // Gather global totals on rank 0
     double maxTime;
-    MPI_Reduce(&localTime, &maxTime, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
+    unsigned long long globalLecturas = 0, globalEscrituras = 0;
+    MPI_Reduce(&totalLecturas, &globalLecturas, 1, MPI_UNSIGNED_LONG_LONG, MPI_SUM, 0, MPI_COMM_WORLD);
+    MPI_Reduce(&totalEscrituras, &globalEscrituras, 1, MPI_UNSIGNED_LONG_LONG, MPI_SUM, 0, MPI_COMM_WORLD);
 
+
+    MPI_Reduce(&localTime, &maxTime, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
+    MPI_Barrier(MPI_COMM_WORLD);
     if (rank == 0) {
-        printf("\nProcesamiento MPI completado.\n");
-        printf("Tiempo máximo entre todos los procesos: %.2f segundos\n", maxTime);
+        double tiempoTotal = maxTime;
+        unsigned long long totalAccesos = globalLecturas + globalEscrituras;
+        double instrucciones = (double)totalAccesos * 20.0 * NUM_THREADS;
+        double mips = (instrucciones / 1e6) / tiempoTotal;
+        double mbps = (double)totalAccesos / (1024.0 * 1024.0) / tiempoTotal;
+
+        char mipsStr[64], formattedMips[64];
+        sprintf(mipsStr, "%.2f", mips);
+        formatNumberWithCommas(mipsStr, formattedMips);
+
+        char lecStr[64], escStr[64];
+        sprintf(lecStr, "%llu", globalLecturas);
+        sprintf(escStr, "%llu", globalEscrituras);
+
+        char formattedLecturas[64], formattedEscrituras[64];
+        formatNumberWithCommas(lecStr, formattedLecturas);
+        formatNumberWithCommas(escStr, formattedEscrituras);
+
+        int minutes = (int)tiempoTotal / 60;
+        double seconds = tiempoTotal - (minutes * 60);
+
+        FILE *finalLog = fopen("final_log.txt", "w");
+        if (finalLog) {
+            fprintf(finalLog, "--- Reporte Final ---\n");
+            fprintf(finalLog, "Kernel utilizado para desenfoque: %d\n", kernelSize);
+            fprintf(finalLog, "Total de localidades leídas: %s\n", formattedLecturas);
+            fprintf(finalLog, "Total de localidades escritas: %s\n", formattedEscrituras);
+            fprintf(finalLog, "Tiempo total de ejecución: %d minutos con %.2f segundos\n", minutes, seconds);
+            fprintf(finalLog, "Velocidad de procesamiento: %.2f MB/s\n", mbps);
+            fprintf(finalLog, "MIPS estimados: %s\n", formattedMips);
+            fclose(finalLog);
+            printf("\nResumen escrito en final_log.txt\n");
+        } else {
+            fprintf(stderr, "No se pudo escribir final_log.txt\n");
+        }
     }
 
     MPI_Finalize();
@@ -152,7 +198,7 @@ void formatNumberWithCommas(const char *numStr, char *buffer) {
     }
 
     if (strlen(decPart) > 0) {
-        snprintf(buffer, 64, "%s.%s", temp, decPart);
+        snprintf(buffer, 128, "%s.%s", temp, decPart);
     } else {
         strcpy(buffer, temp);
     }
